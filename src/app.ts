17b50234c17802f6d77384b4abe61e464472cb72
Wellington Matheus <w.matheus@outlook.com>
2021-11-05 03:20:22 -0300

    feat(CORS): add origin whitelist

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,24 @@ import cors from 'cors';
 
 import { router } from './routes';
 
+import environment from '@config/environment';
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    const originWhitelist = environment.ORIGIN_WHITELIST.split(',');
+
+    if (originWhitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`Origin not allowed by CORS`));
+    }
+  }
+};
+
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(cors(corsOptions));
 
 export { app };
