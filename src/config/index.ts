43bcc174daca33a7d4856c547f4e5525e2cec142
Wellington Matheus <w.matheus@outlook.com>
2021-11-05 02:00:08 -0300

    feat(Babel): create Config alias

--- src/config/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import enviromentConfig from './environment';
-
-export type {
-  EnvironmentConfigVariable,
-  EnvironmentConfig
-} from './environment';
-
-export { enviromentConfig as environment };
